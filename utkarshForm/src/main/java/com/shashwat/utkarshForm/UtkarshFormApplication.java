42ab1abb615fc05e6767bd84a954cde4bbab9812
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-06-05 19:35:43 +0530

    Backend Initial COdebase

---
+++ utkarshForm/src/main/java/com/shashwat/utkarshForm/UtkarshFormApplication.java
@@ -0,0 +1,13 @@
+package com.shashwat.utkarshForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtkarshFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtkarshFormApplication.class, args);
+	}
+
+}
---
+++ utkarshForm/src/test/java/com/shashwat/utkarshForm/UtkarshFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashwat.utkarshForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtkarshFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
