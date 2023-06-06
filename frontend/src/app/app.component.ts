fe8c098fe1884f1fd658dfd15631ba48e058d8d6
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-06-06 17:57:20 +0530

    Basic UI changes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  submitForm(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    // Access the form values
+    const name = this.form.controls['name'].value;
+    const email = this.form.controls['email'].value;
+    const message = this.form.controls['message'].value;
+
+    // Do something with the form values (e.g., send data to a server)
+
+    // Reset the form after submission
+    this.form.reset();
+  }
 }
